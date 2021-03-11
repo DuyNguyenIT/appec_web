166b2269a69307031892e3a75c701c888273c611
trucmaipham <74885957+trucmaipham@users.noreply.github.com>
2021-03-11 14:58:23 +0700

    Chỉnh routes cua admin
    chỉnh route của admin, thêm 1 số route cho chức năng mới

--- appec_website/routes/adminRoute.php
+++ appec_website/routes/adminRoute.php
@@ -4,27 +4,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' =>'App\Http\Middleware\isAdmin'], function (){ 
     Route::group(['prefix' => 'quan-ly','namespace'=>'App\Http\Controllers\Admin'], function () {
-    Route::post('ckeditor/image_upload', 'GVCKEditorController@upload')->name('upload');
     Route::get('/', 'homeController@index');
+    
     //bậc đào tạo
+    //PTTMai có chỉnh
     Route::group(['prefix' => 'bac-dao-tao'], function () {
         Route::get('/', 'bacDaoTaoController@index');
         Route::post('them', 'bacDaoTaoController@them');
         Route::post('sua', 'bacDaoTaoController@sua');
-        Route::get('xoa/{id}', 'bacDaoTaoController@xoa');
+        Route::get('xoa/{maBac}', 'bacDaoTaoController@xoa');
     });
     //ngành học
     Route::group(['prefix' => 'nganh-hoc'], function () {
         Route::get('/', 'nganhController@index');
         Route::post('them', 'nganhController@them');
-        Route::post('sua/', 'nganhController@sua');
+        Route::post('sua', 'nganhController@sua');
         Route::get('xoa/{maNganh}', 'nganhController@xoa');
     });
+    //PTTMai thêm 
+    //chuyên ngành
+    Route::group(['prefix' => 'chuyen-nganh'], function () {
+        Route::get('/', 'cNganhController@index');
+        Route::post('them', 'cNganhController@them');
+        Route::post('sua', 'cNganhController@sua');
+        Route::get('xoa/{maCNganh}', 'cNganhController@xoa');
+    });
+    //Hết PTTMai thêm
     //hệ
     Route::group(['prefix' => 'he'], function () {
         Route::get('/', 'heController@index');
-        Route::post('/them', 'heController@them');
-        Route::post('/sua', 'heController@sua');
+        Route::post('them', 'heController@them');
+        Route::post('sua', 'heController@sua');
         Route::get('xoa/{maHe}', 'heController@xoa_he');
     });
 
@@ -55,24 +65,12 @@ Route::group(['middleware' =>'App\Http\Middleware\isAdmin'], function (){
         Route::get('xoa/{maLoaiHocPhan}', 'loaiHocPhanController@xoa');
     });
 
-    //loại học phần
+    //học phần: PTTMai có bổ sung
     Route::group(['prefix' => 'hoc-phan'], function () {
         Route::get('/', 'hocPhanController@index');
-        Route::post('them', 'hocPhanController@them_hp');
-        Route::prefix('de-cuong-mon-hoc')->group(function () {
-            Route::get('/{maHocPhan}','hocPhanController@de_cuong_mon_hoc');
-            Route::post('/them_chuan_dau_ra_mon_hoc','hocPhanController@them_chuan_dau_ra_mon_hoc');
-            
-            Route::post('/sua_mo_ta_mon_hoc','hocPhanController@them_mo_ta_hoc_phan');
-            Route::post('/sua_yeu_cau_mon_hoc','hocPhanController@them_yeu_cau_hoc_phan');
-
-            Route::post('/sua_giao_trinh','hocPhanController@them_giao_trinh');
-            Route::post('/sua_tai_lieu_tham_khao_them','hocPhanController@them_tai_lieu_tham_khao_them');
-            Route::post('/sua_tai_lieu_khac','hocPhanController@them_tai_lieu_khac');
-            Route::post('/them_phuong_phap_giang_day','hocPhanController@them_phuong_phap_giang_giay');
-            Route::post('/them_mon_tien_quyet','hocPhanController@them_mon_tien_quyet');
-            Route::post('/them_noi_dung_mon_hoc','hocPhanController@them_noi_dung_mon_hoc');
-        });
+        Route::post('them', 'hocPhanController@them');
+        Route::post('sua', 'hocPhanController@sua');
+        Route::get('xoa/{maHocPhan}', 'hocPhanController@xoa');
         // Route::post('sua/{id}', '');
         // Route::post('xoa/{id}', '');
     });
@@ -88,8 +86,8 @@ Route::group(['middleware' =>'App\Http\Middleware\isAdmin'], function (){
     //phương pháp giảng dạy
     Route::group(['prefix' => 'phuong-phap-giang-day'], function () {
         Route::get('/', 'ppGiangDayController@index');
-        Route::post('/them', 'ppGiangDayController@them');
-        Route::post('/sua', 'ppGiangDayController@sua');
+        // Route::post('them', '');
+        // Route::post('sua/{id}', '');
         // Route::post('xoa/{id}', '');
     });
 });
