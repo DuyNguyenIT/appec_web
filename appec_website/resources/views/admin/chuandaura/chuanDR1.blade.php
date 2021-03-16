95877e03b18e206fb6018cb375467b788bcfe32b
trucmaipham <74885957+trucmaipham@users.noreply.github.com>
2021-03-16 08:49:58 +0700

    thêm chức năng cho CDR1

--- appec_website/resources/views/admin/chuandaura/chuanDR1.blade.php
+++ appec_website/resources/views/admin/chuandaura/chuanDR1.blade.php
@@ -7,15 +7,15 @@
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1 class="m-0 text-dark">
-              Chuẩn đầu ra CDIO1<noscript></noscript>
+              Level-1 Student Outcomes Management<noscript></noscript>
               <nav></nav>
             </h1>
           </div>
           <!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
-              <li class="breadcrumb-item active">Chuẩn đầu ra 1</li>
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Level-1 Student Outcomes</li>
             </ol>
           </div>
           <!-- /.col -->
@@ -30,14 +30,14 @@
       @if(session('success'))
         <div class="alert alert-success alert-dismissible">
           <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-          <h5><i class="icon fas fa-check"></i> Thông báo!</h5>
+          <h5><i class="icon fas fa-check"></i> Message!</h5>
           {{session('success')}}
         </div>
       @endif
       @if(session('warning'))
           <div class="alert alert-warning alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h5><i class="icon fas fa-exclamation-triangle"></i> Thông báo!</h5>
+            <h5><i class="icon fas fa-exclamation-triangle"></i>Notification!</h5>
             {{session('warning')}}
           </div>
       @endif
@@ -53,67 +53,50 @@
                 <h3 class="card-title">
 
               <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                        <i class="fas fa-plus"></i>Thêm
+                        <i class="fas fa-plus"></i>Add
               </button>
 
                 <!-- Modal -->
                 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                   <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Thêm chuẩn đầu ra</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                      </button>
-                    </div>
-                    <div class="modal-body">
-                      
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
-                      <button type="button" class="btn btn-primary">Lưu</button>
-                    </div>
-                  </div>
-                  </div>
+                    <form action="{{ asset('quan-ly/chuan-dau-ra/them') }}" method="post">
+                    @csrf
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLabel">Adding a new Level-1 Student Outcomes</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <div class="form-group">
+
+                            <label for="">Level-1 Student Outcome ID (number):</label>
+                            <input type="text" name="maCDR1VB" class="form-control" placeholder="">
+                          </div>
+                            <div class="form-group">
+                              <label for="">Level-1 Student Outcome Name:</label>
+                              <input type="text" name="tenCDR1" class="form-control" placeholder="">
+                            </div>
+                        </div>
+                        <div class="modal-footer"> 
+                          <button type="submit" class="btn btn-primary">Save</button>
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        </div>
+                      </div>
+                    </form>
                 </div>
-                 
+                </div> 
                 </h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
-                <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                      <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">
-                              Thêm học phần
-                            </h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">×</span>
-                            </button>
-                          </div>
-                          <div class="modal-body">
-                            <div class="form-group">
-                              <label for="">Nhập tên chuẩn đầu ra:</label>
-                              <input type="text" class="form-control" placeholder="">
-                            </div>
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-primary">
-                              Lưu
-                            </button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                              Hủy
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div><table id="example2" class="table table-bordered table-hover">
+                <table id="example2" class="table table-bordered table-hover">
                   <thead>
                     <tr>
-                      <th>STT</th>
-                      <th>Tên chuẩn đầu ra</th>
-                      <th>Tùy chọn</th>
+                      <th>No.</th>
+                      <th>Level-1 Student Outcomes Name</th>
+                      <th>Management Functions</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -125,17 +108,46 @@
                         <td>{{$i++}}</td>
                         <td>{{$cdr->tenCDR1}}</td>
                         <td>
-                          
-                            <button class="btn btn-success" data-toggle="modal" data-target="#addModal">
-                              <i class="fas fa-align-justify"></i> chỉnh sửa
+                          <button title="Edit" class="btn btn-success" data-toggle="modal" data-target="#edit_{{$cdr->maCDR1}}">
+                            <i class="fas fa-edit"></i> 
+                          </button>
+                          <a title="Delete" class="btn btn-danger" onclick="return confirm('Do you want ro delete {{$cdr->tenCDR1}}?')" href="{{ asset('quan-ly/chuan-dau-ra/xoa/'.$cdr->maCDR1) }}"><i class="fa fa-trash"></i></a>
+                          <a href="{{ asset('/quan-ly/chuan-dau-ra/chuan-dau-ra-2/'.$cdr->maCDR1) }}">
+                              <!-- <button class="btn btn-primary">Chuẩn đầu ra 2</button>-->
+                            <button title="Level-2 Student Outcomes Management"  class="btn btn-success" data-toggle="modal" data-target="#addModal">
+                                <i class="fas fa-align-justify"></i>Level-2 Student Outcomes
                             </button>
-                            
-                            <a href="{{ asset('/quan-ly/chuan-dau-ra/chuan-dau-ra-2/'.$cdr->maCDR1) }}">
-                              <button class="btn btn-primary">
-                              Chuẩn đầu ra 2
-                              </button>
                           </a>  
-                          
+                         
+                              <div class="modal fade" id="edit_{{$cdr->maCDR1}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                  <form action="{{ asset('quan-ly/chuan-dau-ra/sua') }}" method="post">
+                                  @csrf
+                              
+                                  <div class="modal-content">
+                                    <div class="modal-header">
+                                      <h5 class="modal-title" id="exampleModalLabel">
+                                        Editing Level-1 Student Outcome Information
+                                      </h5>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">×</span>
+                                      </button>
+                                    </div>
+                                    <div class="modal-body">
+                                      <input type="text" name="maCDR1" value="{{$cdr->maCDR1}}" class="form-control" hidden>
+                                      <div class="form-group">
+                                        <label for="">Level-1 Student Outcome Name</label>
+                                        <input type="text" name="tenCDR1" class="form-control" value="{{$cdr->tenCDR1}}">
+                                      </div>
+                                    </div> <!-- end modal-body-->
+                                  </form>
+                                    <div class="modal-footer">
+                                      <button type="submit" class="btn btn-primary">Update</button>
+                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                    </div> 
+                                </div>
+                              </div>
+                          </div>
                         </td>
                       </tr>
                     @endforeach
